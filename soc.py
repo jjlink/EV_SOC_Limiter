e67ed9081392657c5ba5e50dbecde2e6008bcfd0
jjlink <jjlink@users.noreply.github.com>
2018-10-02 16:03:56 -0700

    Added exception days
    needed because I may not be able to charge at work on these days.

--- soc.py
+++ soc.py
@@ -7,67 +7,78 @@ import datetime
 
 # soc.bash
 #rm soc.json
-#curl  -X GET -c cookiejar https://tmc.openvehicles.com:6869/api/cookie\?username=XXXXX\&password=XXXXXXXX
-#curl  -X GET -b cookiejar 'https://tmc.openvehicles.com:6869/api/charge/P-XXXXXX' >soc.json
+#curl  -X GET -c cookiejar https://api.openvehicles.com:6869/api/cookie\?username=jjlink\&password=plugnev2012\!\!\!
+#curl  -X GET -b cookiejar 'https://api.openvehicles.com:6869/api/charge/P-1310025' >soc.json
 #cat soc.json
 
 # vera device ON - bash veraOn.sh
-#curl -X GET http://192.168.1.104:3480/data_request?id=action\&output_format=json\&serviceId=urn:micasaverde-com:serviceId:HomeAutomationGateway1\&action=RunScene\&SceneNum=54
+#curl -X GET http://192.168.1.44:3480/data_request?id=action\&output_format=json\&serviceId=urn:micasaverde-com:serviceId:HomeAutomationGateway1\&action=RunScene\&SceneNum=01
 
 # vera device OFF - bash veraOFF.sh
-#curl -X GET http://192.168.1.104:3480/data_request?id=action\&output_format=json\&serviceId=urn:micasaverde-com:serviceId:HomeAutomationGateway1\&action=RunScene\&SceneNum=55
+#curl -X GET http://192.168.1.44:3480/data_request?id=action\&output_format=json\&serviceId=urn:micasaverde-com:serviceId:HomeAutomationGateway1\&action=RunScene\&SceneNum=02
 
 target = 80
 
-
-
-bashCommand = "rm nohup.out"
-os.system(bashCommand)
+#bashCommand = "rm nohup.out"
+#os.system(bashCommand)
 bashCommand = "bash veraON.sh"
 os.system(bashCommand)
 
 while True:
 
-    print("\n"+str(datetime.datetime.now())+"\n")
-    bashCommand = "bash soc.sh"
-    os.system(bashCommand)
+	print("\n"+str(datetime.datetime.now())+"\n")
+	bashCommand = "bash soc.sh"
+	os.system(bashCommand)
  
-    with open('soc.json') as data_file:   
-    	data = json.load(data_file)
+	with open('soc.json') as data_file:   
+	    data = json.load(data_file)
  
-    #pprint(data)
+	#pprint(data)
  
-    soc = int(data["soc"])
-    chargestate = str(data["chargestate"])
+	soc = int(data["soc"])
+	chargestate = str(data["chargestate"])
  
-    print("\nSOC= "+str(soc)+" Target= "+str(target)+"\n")
+	print("\nSOC= "+str(soc)+" Target= "+str(target)+"\n")
 
-    if chargestate == "done" or chargestate == "stopped":
-   	 print("Car is NOT charging.\n\n")
-   	 bashCommand = "rm nohup.out"
-   	 os.system(bashCommand)
-   	 time.sleep((60 * 15))  # Delay for 15 minutes
+	if chargestate == "done" or chargestate == "stopped":
+		print("Car is NOT charging.\n\n")
+#		bashCommand = "rm nohup.out"
+#		os.system(bashCommand)
+		time.sleep((60 * 15))  # Delay for 15 minutes
 
-    elif chargestate == "charging":
-   	 if soc >= target:
-   		 print("SOC is " + str(soc) + "%, Charge Limit of " + str(target) + "% Reached.\n")
-   		 print("shut off power outlet.\n")
-   		 bashCommand = "bash veraOFF.sh"
-   		 os.system(bashCommand)
-   		 bashCommand = "rm nohup.out"
-   		 os.system(bashCommand)
-   		 break
-   		 #time.sleep((60 * 720))  # Delay for 12 hours.
-   	 else:
-   		 print("SOC is " + str(soc) + "%, Continue Charging to " +str(target) + "%\n")
-   		 print("wait a while then go get more data from OVMS server.\n")
-    else:
-   	 print("Invalid jason file.")
+	elif chargestate == "charging":
+		if soc >= target:
+			print("SOC is " + str(soc) + "%, Charge Limit of " + str(target) + "% Reached.\n")
+			print("shut off power outlet.\n")
+			bashCommand = "bash veraOFF.sh"
+			os.system(bashCommand)
+			bashCommand = "rm nohup.out"
+			os.system(bashCommand)
+			break
+			#time.sleep((60 * 720))  # Delay for 12 hours.
+		else:
+			now = datetime.datetime.now()
+			
+			if  now.weekday() == 1 and now.hour < 5:
+				print("its Tuesday morning, so lets break..\n")
+				break	
+			elif now.weekday() == 2 and now.hour < 5:
+				print("its Wednesday morning, so lets break..\n")
+				break	
+			elif now.weekday() == 3 and now.hour < 5:
+				print("its Thursday morning, so lets break.\n")
+				break	
+			else
+				print("SOC is " + str(soc) + "%, Continue Charging to " +str(target) + "%\n")
+				print("wait a while then go get more data from OVMS server.\n")
+	else:
+		soc = 0 # see if this helps!!!
+		print("Invalid jason file.")
 
-   	 
-    if (soc + 5) >= target:
-    	time.sleep((15 * 1))  # Delay for 15 seconds
-    else:
-    	time.sleep((60 * 1))  # Delay for 1 minute
+		
+	if (soc + 5) >= target:
+	    time.sleep((15 * 1))  # Delay for 15 seconds
+	else:
+	    time.sleep((60 * 1))  # Delay for 1 minute
 
 print('\nEnd of program.\n')
